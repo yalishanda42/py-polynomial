a7df1e98212372f82e4f44102464af0ac7312a6c
Alexander Ignatov <ani1802@abv.bg>
2020-02-24 01:28:54 +0200

    Change package name and bump version to 0.2

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from distutils.core import setup
 setup(
-  name = 'py-polynomial',
-  packages = ['py-polynomial'],
-  version = '0.1',
+  name='py-polynomial',
+  packages=['pypolynomial'],
+  version='0.2',
   license='MIT',
-  description = 'Package handling mathematical single-variable polynomials.',
-  author = 'Alexander Ignatov',
-  author_email = 'yalishanda@abv.bg',
-  url = 'https://github.com/allexks/py-polynomial',
-  download_url = 'https://github.com/allexks/py-polynomial/archive/0.1.tar.gz',
-  keywords = ['polynomial', 'maths', 'derivative', 'roots', 'algebra', 'linear'],
+  description='Package handling mathematical single-variable polynomials.',
+  author='Alexander Ignatov',
+  author_email='yalishanda@abv.bg',
+  url='https://github.com/allexks/py-polynomial',
+  download_url='https://github.com/allexks/py-polynomial/archive/0.2.tar.gz',
+  keywords=['polynomial', 'maths', 'derivative', 'roots', 'algebra', 'linear'],
   install_requires=[],
   classifiers=[
     'Development Status :: 3 - Alpha',
@@ -20,5 +20,7 @@ setup(
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
   ],
 )
