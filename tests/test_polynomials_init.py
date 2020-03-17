ed9785fee920d60b41159b41b0025865dd187b9b
Alexander Ignatov <ani1802@abv.bg>
2020-03-18 00:33:05 +0200

    Remove polynomials submodule

--- tests/test_polynomials_init.py
+++ tests/test_polynomials_init.py
@@ -1,7 +1,7 @@
 """Unit-testing module defining polynomials initialization test cases."""
 
 import unittest
-from polynomial.polynomials import *
+from polynomial import *
 
 
 class TestPolynomialsInit(unittest.TestCase):
