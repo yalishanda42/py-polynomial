a249ef0a53420cf057f25ab9ed6b12fe2abd09ae
philippeitis <33013301+philippeitis@users.noreply.github.com>
2020-03-19 15:39:30 -0700

    Remove redundant tests, change __str__ to __repr__

--- tests/test_polynomials_repr.py
+++ tests/test_polynomials_repr.py
@@ -1,4 +1,4 @@
-"""Unit-testing module defining polynomials __str__ test cases."""
+"""Unit-testing module defining polynomials __repr__ test cases."""
 
 import unittest
 from polynomial import (
@@ -12,48 +12,12 @@ from polynomial import (
 
 
 class TestPolynomialsRepr(unittest.TestCase):
-    """Defines polynomials __str__ test cases."""
+    """Defines polynomials __repr__ test cases."""
 
     def test_smoke(self):
         """Perform a test case containing all possible complicated inputs."""
         coeffs = [0, 0, -2, 0, 4.6666666666, -(69 + 420j), -1, 1337]
         expect = "Polynomial(-2, 0, 4.6666666666, (-69-420j), -1, 1337)"
-
-        r = repr(Polynomial(coeffs))
-
-        self.assertEqual(expect, r)
-
-    def test_negative_coefficient_unchanged(self):
-        """Test that negative coefficients replace the + between the terms."""
-        coeffs = [3, -2, 0, 0]
-        expect = "Polynomial(3, -2, 0, 0)"
-
-        r = repr(Polynomial(coeffs))
-
-        self.assertEqual(expect, r)
-
-    def test_trailing_zero_kept(self):
-        """Test that the first-degree-term ends with 'x' and not 'x^1'."""
-        coeffs = [2, 0]
-        expect = "Polynomial(2, 0)"
-
-        r = repr(Polynomial(coeffs))
-
-        self.assertEqual(r, expect)
-
-    def test_constant_kept(self):
-        """Test that the constant term does not end with 'x' or 'x^0'."""
-        coeffs = [2]
-        expect = "Polynomial(2)"
-
-        r = repr(Polynomial(coeffs))
-
-        self.assertEqual(expect, r)
-
-    def test_internal_zero_kept(self):
-        """Test that constants with value 1 are displayed properly."""
-        coeffs = [1, 0, 1]
-        expect = "Polynomial(1, 0, 1)"
 
         r = repr(Polynomial(coeffs))
 
